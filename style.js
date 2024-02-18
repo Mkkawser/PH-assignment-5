9d0bd6593e36bb5c7dfc2ca653f74cd1833cf09a
Mkkawser <irumorgan420@gmail.com>
2024-02-18 21:46:40 +0600

    dynamic price update

--- style.js
+++ style.js
@@ -1,21 +1,55 @@
 const cards = document.querySelectorAll(".cards");
 const seatCount = document.getElementById("seat-count");
-let totalSeat = 40;
-let seatName = [];
+const total = document.getElementById("total-price");
+const totalSeat = document.querySelector(".totalSeat");
+let seatName = new Set();
 let seat = 0;
+let totalPrice = 0;
+
+let seatNames = document.querySelector(".seatName");
+let seatClass = document.querySelector(".seatClass");
+let seatPrice = document.querySelector(".seatPrice");
 
 for (let i = 0; i < cards.length; i++) {
   const card = cards[i];
   card.addEventListener("click", function () {
-    seatName.push(cards.innerHTML);
+    if (seat > 3) {
+      alert(`Can't Occopid more than 4 seat`);
+      return;
+    }
+
     if (card.style.backgroundColor == "rgb(29, 209, 0)") {
       card.style.backgroundColor = "#f2f2f2";
-      seat = seat + 1;
-    } else card.style.backgroundColor = "rgb(29, 209, 0)";
-  });
-  seatCount.innerText = seat;
-}
+      seat -= 1,totalPrice-=550;
+      seatName.delete(card.innerHTML);
+    } else {
+      seat += 1,totalPrice+=550;
+      card.style.backgroundColor = "rgb(29, 209, 0)";
+      seatName.add(card.innerHTML);
+    }
+    console.log(seat,totalPrice);
+    seatCount.innerText = seat;
+    totalSeat.innerHTML = 40 - seat;
+    total.innerHTML=totalPrice;
 
-seatCount.addEventListener("change", function () {
-  console.log(seatCount.innerHTML);
-});
+
+    // Select Your Seat
+    console.log(seatName);
+    seatNames.innerHTML = "";
+    seatPrice.innerHTML = "";
+    seatClass.innerHTML = "";
+    for (const value of seatName) {
+      const p = document.createElement("p");
+      p.innerText = value;
+      seatNames.append(p);
+
+      const q = document.createElement("p");
+      q.innerText = "Bussiness";
+      seatClass.append(q);
+
+      const r = document.createElement("p");
+      r.innerText = 550;
+      seatPrice.append(r);
+    }
+  });
+}

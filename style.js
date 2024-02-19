bcac69606413932c919b9d10b91c58cfcae4882b
Mkkawser <irumorgan420@gmail.com>
2024-02-19 19:43:23 +0600

    card function updated

--- style.js
+++ style.js
@@ -29,11 +29,13 @@ for (let i = 0; i < cards.length; i++) {
 
     if (card.style.backgroundColor == "rgb(29, 209, 0)") {
       card.style.backgroundColor = "#f2f2f2";
+      card.style.color = "black";
       (seat -= 1), (totalPrice -= 550);
       seatName.delete(card.innerHTML);
     } else {
       (seat += 1), (totalPrice += 550);
       card.style.backgroundColor = "rgb(29, 209, 0)";
+      card.style.color = "white";
       seatName.add(card.innerHTML);
     }
     // console.log(seat,totalPrice);
@@ -53,46 +55,47 @@ for (let i = 0; i < cards.length; i++) {
       seatNames.append(p);
 
       const q = document.createElement("p");
-      q.innerText = "Bussiness";
+      q.innerText = "Economoy";
       seatClass.append(q);
 
       const r = document.createElement("p");
       r.innerText = 550;
       seatPrice.append(r);
     }
+    if (totalPrice / 550 == 4)
+      document.querySelector(".btndis").disabled = false;
   });
+  document.querySelector(".btndis").disabled = true;
 }
 
 function couponCode(e) {
   let x = Number(total.innerHTML);
   let y = coupon.value;
-  if (x == 0) {
-    alert("Percess First");
-    return;
-  }
+  console.log("coupon", y);
+
   if (coupon.value == "NEW15") {
     couponPrice = Math.round(totalPrice * 0.15);
-    document.querySelector(".btndis").disabled = true;
-  }
-  if (coupon.value == "Couple 20") {
+    document.querySelector(".btndis").style.display = "none";
+    document.querySelector(".coupon").style.display = "none";
+  } else if (coupon.value == "Couple 20") {
     couponPrice = Math.round(totalPrice * 0.2);
-    document.querySelector(".btndis").disabled = true;
-  }
-  couponOff.innerHTML = `OFF (${y})`;
+    document.querySelector(".btndis").style.display = "none";
+    document.querySelector(".coupon").style.display = "none";
+  } else alert("Invalid Coupon Code");
+  couponOff.innerHTML = `Discount Price (${y})`;
   couponOffP.innerHTML = -couponPrice;
   grand.innerHTML = grand.innerHTML - couponPrice;
 }
 
 mobileNumber.addEventListener("change", function (e) {
   let x = e.target.value;
-  if (x.length == 11) {
+  // used 5 for simplicity
+  if (x.length >= 5) {
     document.querySelector(".confirmdis").disabled = false;
   }
+  if (totalPrice == 0) document.querySelector(".confirmdis").disabled = true;
 });
 
 function confirmBox() {
   // document.querySelector(".confirmdis").disabled = true;
-}
-function showModal(){
-  
 }
